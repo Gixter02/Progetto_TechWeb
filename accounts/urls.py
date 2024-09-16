410dfb80a08377fb57e14725f959f08ea43cfb5a
Gixter02 <mattia.montanari2002@gmail.com>
2024-09-16 12:40:45 +0200

    Aggiunta vista listautenti

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', home_accounts, name='home_accounts'),
+    path('utenti/', ListaUtentiView.as_view(), name='lista_utenti'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from accounts.models import Utente
+
 
 # Create your views here.
 def home_accounts(request):
     render(request, template_name= "accounts/base.html", context={})
+
+class ListaUtentiView(ListView):
+    model = Utente
+    template_name = "accounts/listautenti.html"
