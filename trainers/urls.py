39fb876518fe238ab37963a560dfc69b48222c93
Gixter02 <mattia.montanari2002@gmail.com>
2024-09-26 18:20:54 +0200

    Aggiunto list e detail view con recensioni per allenatori

--- trainers/urls.py
+++ trainers/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from .views import PersonalTrainerCreateView, PersonalTrainerUpdateView, PersonalTrainerDetailView, \
-    PersonalTrainerHomeView, PersonalTrainerListView, allenamenti_programmati
+from .views import PersonalTrainerCreateView, PersonalTrainerUpdateView, \
+    PersonalTrainerHomeView, allenamenti_programmati, PersonalTrainerReviewDetailView, \
+    elenco_personal_trainer
 
 app_name = 'trainers'
 
@@ -11,9 +12,9 @@ urlpatterns = [
     path('', PersonalTrainerHomeView.as_view(), name='personal_trainer_home'),
     path('create/', PersonalTrainerCreateView.as_view(), name='personal_trainer_create'),
     path('<int:pk>/edit/', PersonalTrainerUpdateView.as_view(), name='personal_trainer_update'),
-    path('<int:pk>/', PersonalTrainerDetailView.as_view(), name='personal_trainer_detail'),
-    path('elencoallenatori/', PersonalTrainerListView.as_view(), name='personal_trainer_list'),
     path('allenamenti/', allenamenti_programmati, name='allenamenti_programmati'),
+    path('detail/<pk>/', PersonalTrainerReviewDetailView.as_view(), name='personal_trainer_review'),
+    path('elenco/', elenco_personal_trainer, name='elenco_personal_trainer'),
 ]
 
 if settings.DEBUG:
--- trainers/views.py
+++ trainers/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DetailView, TemplateView, ListView
 
 from bookings.models import Prenotazione
+from reviews.models import Recensione
 from .models import PersonalTrainer
 from .forms import PersonalTrainerForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -36,14 +38,6 @@ class PersonalTrainerUpdateView(LoginRequiredMixin, UpdateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-# View per visualizzare i dettagli del Personal Trainer
-class PersonalTrainerDetailView(LoginRequiredMixin, DetailView):
-    model = PersonalTrainer
-    template_name = 'trainers/personal_trainer_detail.html'
-
-    def get_queryset(self):
-        # Mostra solo i dettagli del personal trainer autenticato
-        return PersonalTrainer.objects.filter(user=self.request.user)
 
 class PersonalTrainerHomeView(TemplateView):
     template_name = 'trainers/personal_trainer_home.html'
@@ -59,9 +53,6 @@ class PersonalTrainerHomeView(TemplateView):
             context['personal_trainer'] = None
         return context
 
-class PersonalTrainerListView(ListView):
-    model = PersonalTrainer
-    template_name = 'trainers/personal_trainer_list.html'
 
 @login_required
 def allenamenti_programmati(request):
@@ -77,3 +68,25 @@ def allenamenti_programmati(request):
     allenamenti = Prenotazione.objects.filter(personal_trainer=personal_trainer).order_by('data_prenotazione', 'fascia_oraria')
 
     return render(request, 'trainers/allenamenti_programmati.html', {'allenamenti': allenamenti})
+
+class PersonalTrainerReviewDetailView(DetailView):
+    model = PersonalTrainer
+    template_name = 'trainers/personal_trainer_review_detail.html'
+    context_object_name = 'personal_trainer'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Calcolare la media delle recensioni
+        context['media_voti'] = Recensione.objects.filter(personal_trainer=self.object).aggregate(Avg('voto'))['voto__avg']
+        # Recuperare tutte le recensioni dell'allenatore
+        context['recensioni'] = Recensione.objects.filter(personal_trainer=self.object)
+        return context
+
+def elenco_personal_trainer(request):
+    # Ottieni tutti i personal trainer e la loro media voti
+    personal_trainer_list = PersonalTrainer.objects.annotate(media_voti=Avg('recensione__voto'))
+
+    context = {
+        'personal_trainer_list': personal_trainer_list,
+    }
+    return render(request, 'trainers/personal_trainer_elenco.html', context)
